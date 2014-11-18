3b3627b0d335a4361286718c1907032370bdd714
cengq <cengq314@gmail.com>
2014-11-18 13:19:57 +0800

    simulation change

--- 统计推断/simulation2.r
+++ 统计推断/simulation2.r
@@ -56,39 +56,49 @@ invF <- function(x){                     # define the invers cdf
 invF = Vectorize(invF)
 
 plot(f,0,2)
-plot(FF,0,1)
+plot(FF,0,2,ylab = 'F')
 
 mu = integrate(function(x) f(x)*x, 0, 2)              # solve Q2
 sigma = integrate(function(x) f(x)*(x - 7/6)^2, 0, 2)
 
 
-U = runif(1000)                                  # Direct metho
+U = runif(1000)                                  # Direct method
 X1 = invF(U)
 mu1 <- mean(X1)
-plot(f, 0, 2)
-hist(X1,breaks = 50,freq = FALSE ,add = TRUE)
+plot(f, 0, 2,main = 'Direct method')
+hist(X1,breaks = 20,freq = FALSE ,add = TRUE)
                                     
 X2 <- runif(2800,0,2)                 # Accept-Reject
 Y <- runif(2800,0,4/3)
 X2 <- X2[Y < f(X2)]
 X2 <- X2[1:1000]
 mu2 <- mean(X2)
-plot(f, 0, 2)
-hist(X2,breaks = 50,freq = FALSE ,add = TRUE)
+plot(f, 0, 2, main = 'Accept-Reject')
+hist(X2,breaks = 20,freq = FALSE ,add = TRUE)
 
-print("wait...")
+print('wait...')
 
-#repeat Direct method£¬using Vectorize and the apply function to speed up.
-# using proc.time to record the time consumption
-# stime <- proc.time()
-# X1_mat <- matrix(invF(runif(1000*2000)),nrow = 2000 , ncol = 1000)  
-# Mu1 = apply(X1_mat,1,mean)
-# proct1 <- proc.time() - stime
+#repeat Direct method ,using Vectorize and the apply function to speed up.
+#using proc.time to record the time consumption
+stime <- proc.time()
+X1_mat <- matrix(invF(runif(1000*2000)),nrow = 2000 , ncol = 1000)  
+Mu1 = apply(X1_mat,1,mean)
+proct1 <- proc.time() - stime
 
 #repeat Accept-Reject method,
-X2 <- runif(2700*2000,0,2) 
-Y <- runif(2700*2000,0,4/3)
+stime <- proc.time()
+X2 <- runif((2670*2000),0,2) 
+Y <- runif((2670*2000),0,4/3)
 X2 <- X2[Y < f(X2)]
-X2 <- X2[1:1000*2000]
+X2 <- X2[1:(1000*2000)]
 X2_mat <- matrix(X2,nrow = 2000 , ncol = 1000)  
 Mu2 = apply(X2_mat,1,mean)
+proct2 <- proc.time() - stime
+
+#relation between mu_bar and mu
+Mu_1 = sqrt(1000)*(Mu1 - 7/6)/sqrt(19/72)
+plot(dnorm, -3, 3,main = 'Direct method')
+hist(Mu_1,breaks = 30,freq = FALSE ,add = TRUE)
+Mu_2 = sqrt(1000)*(Mu2 - 7/6)/sqrt(19/72)
+plot(dnorm, -3, 3,main = 'Accept-Reject')
+hist(Mu_2,breaks = 30,freq = FALSE ,add = TRUE)
